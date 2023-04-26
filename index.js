dc45c4b02806a1020837bedf691204b71681d4ce
choco69papa <122781148+choco69papa@users.noreply.github.com>
2023-04-26 09:47:30 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
     // カレンダー
-    $(function () {
-        $('input[name="date"]').datepicker({
-            dateFormat: 'yy年mm月dd日',
-            // 昨日の日付以降を選択できなくする
-            minDate:0,
-        });      
-    }); 
+$(function() {
+  $('input[name="dattes"]').datepicker({
+    dateFormat: 'yy年mm月dd日',
+    minDate: 0,
+    beforeShowDay: function(date) {
+      var day = date.getDay();
+      var weekOfMonth = Math.ceil((date.getDate() - 1) / 7);
+      if (day === 1 || (day === 2 && weekOfMonth === 3)) {
+        return [false];
+      } else {
+        return [true];
+      }
+    }
+  });
+});
+
 
 
     // 予約フォームを表示する
     $('#form-number').click(function () {
         $('#form-name').empty();
-        var date = $('input[name="date"]').val();
+        var date = $('input[name="dattes"]').val();
         var num = $('input[name="number"]:checked').val();
         var minute = $('select[name="minute"]').val();
         var names = $('select[name="names"]').val();
@@ -21,13 +30,13 @@
 
     // 送信
     $('form').submit(function () {
-        var date = $('input[name="date"]').val();
+        var date = $('input[name="dattes"]').val();
         var number = $('input[name="number"]:checked').val();	
         var minute = $('select[name="minute"]').val();
         var names = $('select[name="names"]').val();
         var inquiries = $('textarea[name="inquiries"]').val();
 	    
-        var msg = `希望日： \n ${date}\n時間： \n ${minute}\nメニュー： \n ${names}\n問い合わせ内容：\n ${inquiries}`;
+        var msg = `希望日： \n ${dattes}\n時間： \n ${minute}\nメニュー： \n ${names}\n問い合わせ内容：\n ${inquiries}`;
         sendText(msg);
 
         return false;
