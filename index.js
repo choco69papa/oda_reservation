3b27684956ee5ab2c268f4fda916359a6670f6bd
choco69papa <122781148+choco69papa@users.noreply.github.com>
2023-05-23 08:57:38 +0900

    Update index.js

--- index.js
+++ index.js
@@ -15,12 +15,38 @@ $(function() {
     timeFormat: 'HH:mm',
     controlType: 'select',
     oneLine: true,
-    stepMinutes: 60, // 1時間ごとに設定
-    minTime: '9:00', // 最小時刻
-    maxTime: '16:00' // 最大時刻
+    showButtonPanel: false,
+    onSelect: function(dateText, inst) {
+      // 日付の選択時に時刻セレクトボックスを更新
+      updateSelectOptions(inst.input);
+    },
+    onClose: function(dateText, inst) {
+      // カレンダーを閉じた時に時刻セレクトボックスを更新
+      updateSelectOptions(inst.input);
+    }
   });
+
+  // 時刻セレクトボックスのオプションを更新する関数
+  function updateSelectOptions(input) {
+    var selectedDate = $.datepicker.parseDate('yy年mm月dd日', input.value);
+    var startHour = 9; // 開始時刻（9時）
+    var endHour = 16; // 終了時刻（16時）
+    var stepMinutes = 60; // 間隔（1時間）
+
+    // 時刻セレクトボックスをクリア
+    $(input).next('.ui-timepicker-select').empty();
+
+    // 開始時刻から終了時刻までのオプションを追加
+    for (var hour = startHour; hour <= endHour; hour++) {
+      for (var minute = 0; minute < 60; minute += stepMinutes) {
+        var time = ('0' + hour).slice(-2) + ':' + ('0' + minute).slice(-2);
+        $(input).next('.ui-timepicker-select').append($('<option>').val(time).text(time));
+      }
+    }
+  }
 });
 
+	
 
     // 予約フォームを表示する
     $('#form-number').click(function () {
