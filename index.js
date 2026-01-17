73769e4ecddeae94aa2a8c61217a48ac789f92ec
choco69papa <122781148+choco69papa@users.noreply.github.com>
2026-01-18 03:10:39 +0900

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ $(function () {
     const MY_LIFF_ID = "1657883881-JG16djMv"; 
 
     // ② GASのURL
-    const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbyHKMuQcz3ey84rfOVDVmEL8nz-ShPb8xUcooY3xyKuNoxlwvb_ANLPTCpW5AsLbFe7/exec';
+    const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbz0ljDUdwuAoJrWIVN9cHtZNSkrjustT5PiyUZGmMClCleE3nyg7BVClOY8sLbieX5-/exec';
 
     // =================================================================
 
